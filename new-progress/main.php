ab8804c689ff5db076bdb006b9f84be9bc648230
akosikhada <miguel.enrique.dasalla.github@gmail.com>
2025-04-02 19:08:39 +0800

    refactor(main): fix the link

--- new-progress/main.php
+++ new-progress/main.php
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TOUCHFIND | WELCOME PAGE</title>
-    <link rel="stylesheet" href="./css/style.css">
-    <link rel="stylesheet" href="./vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/vendor/bootstrap/css/bootstrap.min.css">
 </head>
 <body>
-    <script src="./vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="./js/script.js"></script>
+    <?php include_once '/components/footer.php'; ?>
+
+    <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/js/script.js"></script>
 </body>
 </html>
