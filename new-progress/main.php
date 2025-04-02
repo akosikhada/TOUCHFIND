08d5fee0e00ab9e5e7eae39841fe0d68e2857aae
akosikhada <miguel.enrique.dasalla.github@gmail.com>
2025-04-02 17:32:00 +0800

    feat: add start-up files

---
+++ new-progress/main.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TOUCHFIND | WELCOME PAGE</title>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./vendor/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+    <script src="./vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="./js/script.js"></script>
+</body>
+</html>
